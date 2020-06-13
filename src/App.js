c7438ab3e11dc9a58063efc457fe07da436e0d2e
xtring <hh940630@gmail.com>
2020-06-14 00:58:27 +0900

    #1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React, { Fragment } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Fragment>
+            <Button danger />
+            <Button />
+        </Fragment>
+    );
 }
+
+const Button = ({ danger }) => (
+    <button
+        className={danger ? 'button button--danger' : 'button button--success'}
+    >
+        Hello
+    </button>
+);
 
 export default App;
