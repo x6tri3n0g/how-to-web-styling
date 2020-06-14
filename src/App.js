ebf6314b7d8e2c78e58518b1a3f8b6761810630e
xtring <hh940630@gmail.com>
2020-06-14 16:36:36 +0900

    #7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import styled, { createGlobalStyle, css } from 'styled-components';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 import React from 'react';
+import theme from './theme';
 
 const GlobalStyle = createGlobalStyle`
   body{
@@ -9,34 +10,37 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const awesomeCard = css`
-    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-    background-color: white;
-    border-radius: 10px;
-    padding: 20px;
-`;
-
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     background-color: pink;
 `;
 
-const Input = styled.input.attrs({
-    required: true,
-})`
-    border: none;
-    border-radius: 5px;
-    ${awesomeCard};
+const Card = styled.div`
+    background-color: white;
+`;
+
+const Button = styled.button`
+    border-radius: 30px;
+    padding: 25px 15px;
+    background-color: ${(props) => props.theme.successColor};
 `;
 
 function App() {
     return (
-        <Container>
-            <GlobalStyle />
-            <Input placeholder="Hello" />
-        </Container>
+        <ThemeProvider theme={theme}>
+            <Container>
+                <GlobalStyle />
+                <Form />
+            </Container>
+        </ThemeProvider>
     );
 }
+
+const Form = () => (
+    <Card>
+        <Button>Hello</Button>
+    </Card>
+);
 
 export default App;
